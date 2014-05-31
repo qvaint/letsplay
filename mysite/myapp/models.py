8ca15bf15c28cb53e35dd51088b9987f9a90b72c
Jason Traff <J@leaky.com>
2014-05-31 17:28:23 -0500

    comment out models _str_
    had to comment it out so that i could manually edit in /admin online

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,12 +5,12 @@ class Question(models.Model):
   platform_choice = models.CharField(max_length=200)
   platform_name = models.CharField(max_length=200)
   game_preference = models.CharField(max_length=200)
-  def __str__(self):
-    return self.question_text
+#  def __str__(self):
+#    return self.question_text
 
 class Result(models.Model):
   question = models.ForeignKey(Question)
-  def __str__(self):
-    return self.result_text
+#  def __str__(self):
+#    return self.result_text
 
     
